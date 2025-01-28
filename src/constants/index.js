0017d8a617c34b092e3fa55b15ede3070a5d1031
Karthik <karthiksrinath23@gmail.com>
2025-01-28 15:32:59 +0530

    First Version

---
+++ src/constants/index.js
@@ -0,0 +1,63 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Tailwind Css, as well as back-end technologies like Django, MySQL, PostgreSQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With an year of professional experience, I have worked with a variety of technologies, including React, Material UI, Django, MySQL, PostgreSQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Python Full Stack Developer",
+    company: "Besant Technologies",
+    description: `Passionate Python Full-Stack Developer with 6 months of hands-on experience in both front-end and back-end development. Skilled in building dynamic web applications using Python, Django, and JavaScript frameworks like React.js. Adept at creating seamless user experiences and robust server-side functionality, with a focus on clean, efficient code and innovative solutions.`,
+    technologies: ["Javascript", "React.js", "Django", "MySQL"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Associate Software Developer",
+    company: "Techfinite Solutions",
+    description: `Enthusiastic Associate Software Developer with a strong foundation in software development principles and hands-on experience in designing, developing, and maintaining applications. familiar in programming languages such as Java, JavaScript, and working with frameworks like Node.js and React Native`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Django", "MySQl"],
+  },
+  {
+    title: "Travel Website",
+    image: project2,
+    description:
+      "An application for exploring destinations, travel packages, and booking options.",
+    technologies: ["HTML", "CSS", "React", "PostgresSQL"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Framer Motion"],
+  },
+  {
+    title: "Crypto Price Tracker",
+    image: project4,
+    description:
+      "A platform that provides real-time data on the prices of various cryptocurrencies. It fetches live information from cryptocurrency exchanges and APIs to display up-to-date prices, market capitalization, trading volume, and price changes ",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "MySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "9-20-39 Third PRR Street, Chinnalapatti, Dindigul - 624301 ",
+  phoneNo: "+91 8825487682 ",
+  email: "karthisrinath2@gmail.com",
+};
